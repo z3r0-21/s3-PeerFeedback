8599c57286f88966691600548cd9d876c9bbafba
Dimitar Prisadnikov <d.prisadnikov@student.fontys.nl>
2021-09-22 14:36:46 +0200

    added the initial java spring poject files

---
+++ src/main/java/com/g3/feedbackApp/FeedbackAppApplication.java
@@ -0,0 +1,13 @@
+package com.g3.feedbackApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g3/feedbackApp/FeedbackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g3.feedbackApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
