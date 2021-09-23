db3997c887f16a4570317aa73bb273fa4aa5340f
Junjie Cen <439102@student.fontys.nl>
2021-09-23 13:57:24 +0200

    test ci from Jj local

--- BE/src/main/java/com/g3/feedbackApp/FeedbackAppApplication.java
+++ BE/src/main/java/com/g3/feedbackApp/FeedbackAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FeedbackAppApplication {
-
+	//this line is just to test the ci
 	public static void main(String[] args) {
 		SpringApplication.run(FeedbackAppApplication.class, args);
 	}
