c7567f06fed14a15c2c84affdd5671c9d13e9bed
Dimitar Prisadnikov <d.prisadnikov@student.fontys.nl>
2021-10-12 09:29:44 +0200

    made the intial FE from scratch since the one we had did not work

---
+++ fe-feedbackapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './Main';
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ fe-feedbackapp/src/Components/Router.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Router(props) {
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default Router;
