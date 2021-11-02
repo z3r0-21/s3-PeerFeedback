b06ceaf1b80d979b56212b585687e07d3a3e7577
Nyagolov,Plamen P.A <a.nyagolov@student.fontys.nl>
2021-11-02 09:47:18 +0100

    fe-comments

---
+++ fe-feedbackapp/src/Components/Single.js
@@ -0,0 +1,36 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { Container, Header, List } from "semantic-ui-react";
+
+// import pkg from 'semantic-ui-react/package.json'
+// import Example from "./example";
+
+// const Single = ({ children }) => (
+//   <Container style={{ margin: 20 }}>
+//     <Header as="h3">This example is powered by Semantic UI React {pkg.version} 😊</Header>
+//     <List bulleted>
+//       <List.Item
+//         as="a"
+//         content="💌 Official documentation"
+//         href="https://react.semantic-ui.com/"
+//         target="_blank"
+//       />
+//       <List.Item
+//         as="a"
+//         content="💡 StackOverflow"
+//         href="https://stackoverflow.com/questions/tagged/semantic-ui-react?sort=frequent"
+//         target="_blank"
+//       />
+//     </List>
+
+//     {children}
+//   </Container>
+// );
+
+// // TODO: Switch to https://github.com/palmerhq/the-platform#stylesheet when it will be stable
+// const styleLink = document.createElement("link");
+// styleLink.rel = "stylesheet";
+// styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+// document.head.appendChild(styleLink);
+
+
