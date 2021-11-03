40778344b25b7afade3fa2e57ae54bf2bd732a62
Maarten Hormes <I456806@fhict.nl>
2021-11-03 13:30:06 +0100

    comment moved to other component

--- fe-feedbackapp/src/Main.js
+++ fe-feedbackapp/src/Main.js
@@ -2,32 +2,15 @@ import React from 'react';
 import CreatePostForm from './Components/CreatePostForm';
 import Footer from './Components/Footer';
 import NavBar from './Components/NavBar';
-<<<<<<< HEAD
-import ReplyBox from './Components/ReplyBox';
-import  Comments  from './Components/Comments';
 
-=======
-import SelectReviewers from './Components/SelectReviewers';
->>>>>>> a3e4b19675cc178c84e16f62eec7c9ea0c2b2df0
+
 
 function Main(props) {
     return (
         <>
-         <NavBar />
-<<<<<<< HEAD
-         <div className ='comment-section'>
-             <div className='comments'>
-             <Comments />
-             </div>
-             <div className='reply-box'>
-             <ReplyBox />
-             </div>
-         </div>
-         
-=======
-         <CreatePostForm/>
->>>>>>> a3e4b19675cc178c84e16f62eec7c9ea0c2b2df0
-         <Footer />   
+            <NavBar />
+            <CreatePostForm />
+            <Footer />
         </>
     );
 }
