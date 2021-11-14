509bd0bb41bbe59678bfc3ba461c3f986a2b5dfd
Takev,Antonio A.M <a.takev@student.fontys.nl>
2021-11-14 23:34:17 +0100

    Navbar + Footer

--- fe-feedbackapp/src/Components/NavBar.js
+++ fe-feedbackapp/src/Components/NavBar.js
@@ -1,9 +1,40 @@
+import { logDOM } from '@testing-library/dom';
 import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import {LinkContainer} from "react-router-bootstrap";
+import { BrowserRouter} from 'react-router-dom';
+import styles from "./css/Navbar.scss";
 
-function NavBar(props) {
+function NavBar() {
     return (
         <>
-            
+        <BrowserRouter>
+            <Navbar collapseOnSelect expand="md" bg="primary" variant="dark">
+            <Container fluid>
+            <Navbar.Brand>
+                
+                <span>Fontys Peer Review</span>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav>
+                    <LinkContainer to="#" exact>
+                        <Nav.Link >Home</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="#" exact>
+                        <Nav.Link >About</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="#" exact>
+                        <Nav.Link >Contacts</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="#" exact className="profile-avatar">
+                        <i className="fas fa-user-circle"/>
+                    </LinkContainer>
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+            </Navbar>
+        </BrowserRouter>
         </>
     );
 }
--- fe-feedbackapp/src/Main.js
+++ fe-feedbackapp/src/Main.js
@@ -2,14 +2,14 @@ import React from 'react';
 import CreatePostForm from './Components/CreatePostForm';
 import Footer from './Components/Footer';
 import NavBar from './Components/NavBar';
-
+import styles from "./Components/css/Main.scss";
 
 
 function Main(props) {
     return (
         <>
             <NavBar />
-            <CreatePostForm />
+            {/* <CreatePostForm /> */}
             <Footer />
         </>
     );
