baa939c063ebd1f92c79cd56aa90382ffda1946f
Nyagolov,Plamen P.A <a.nyagolov@student.fontys.nl>
2021-11-16 21:35:17 +0100

    comments fe

--- fe-feedbackapp/src/Components/Comments.js
+++ fe-feedbackapp/src/Components/Comments.js
@@ -1,37 +1,66 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { FloatingLabel, Form, Button, ListGroup, Badge, Image } from 'react-bootstrap';
+import Thumb from '../Components/Thumb.png'
 
-class Comments extends Component {
+class Comments extends Component{
 
   state = {
     comments: []
 }
 
-        componentDidMount(){
-        axios.get(`http://localhost:8080/comments`).then(res => {
-        const comments = res.data;
-        this.setState({ comments });
-      });
-        }
 
+ componentDidMount(){
+  axios.get(`http://localhost:8080/comments`).then(res => {
+  const comments = res.data;
+  this.setState({ comments });
+});
+  }
 
+render(){
+return(
 
-
-  render() {
-    return (
-         <ul className="collection">
+  <>
+   <ul className="collection">
             {this.state.comments.map(comment => (
-              <li
-                key={comment.commentId}
-                className="collection-item left-align purple lighten-2"
+              <ListGroup as="ol" numbered>
+              <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
               >
-                <p>User ID: {comment.commentId}</p>
-                <p>Feedback: {comment.text}</p>
-              </li>
+                <div className="ms-2 me-auto">
+                  <div><p>User</p></div>
+                  <p>{comment.text}</p>
+                </div>
+                <Badge variant="primary" pill>
+                  14
+                </Badge>
+                <Button className="likebtn" variant="primary" size="sm">
+               Like|
+               <Image className="thumb"
+          roundedCircle
+          src={Thumb}
+        />
+               <span className="visually-hidden">unread messages</span>
+                </Button>    
+              </ListGroup.Item>
+            </ListGroup>
             ))}
           </ul>
-    )
-  }
+
+  
+  <FloatingLabel controlId="floatingTextarea" label="Comments" className="mb-3">
+  </FloatingLabel>Sugest a solution<FloatingLabel controlId="floatingTextarea2" label="Add comment here">
+      <Form.Control
+        as="textarea"
+        placeholder="Leave a comment here"
+        style={{ height: '100px' }} />
+    </FloatingLabel>
+    <Button variant="outline-primary">Submit</Button>{' '}
+    </>
+);
 }
+};
+
 
 export default Comments
--- fe-feedbackapp/src/Main.js
+++ fe-feedbackapp/src/Main.js
@@ -4,7 +4,6 @@ import Footer from './Components/Footer';
 import NavBar from './Components/NavBar';
 
 
-
 function Main(props) {
     return (
         <>
