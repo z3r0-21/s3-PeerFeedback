1591100c1f821317cbd38c460d7b53661c2f73f6
Maarten Hormes <I456806@fhict.nl>
2021-11-17 14:49:48 +0100

    orm maps to azure sql now

--- BE/src/main/java/com/g3/feedbackApp/Models/UserModel.java
+++ BE/src/main/java/com/g3/feedbackApp/Models/UserModel.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 @Setter
 @NoArgsConstructor
 @Entity
-@Table(name ="user")
+@Table(name ="userTable")
 public class UserModel {
 
     @Id
--- BE/src/main/java/com/g3/feedbackApp/Repository/IUserRepository.java
+++ BE/src/main/java/com/g3/feedbackApp/Repository/IUserRepository.java
@@ -3,7 +3,7 @@ package com.g3.feedbackApp.Repository;
 import com.g3.feedbackApp.Models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUserRepository extends JpaRepository<UserModel, Integer> {
+public interface IUserRepository extends JpaRepository<UserModel, Long> {
 
     UserModel getUserModelByStudentNr(Long nr);
     UserModel getUserModelByEmail(String email);
