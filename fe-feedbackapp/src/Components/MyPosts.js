d530f622925891d625a63d804a3f73f524fbb961
Takev,Antonio A.M <a.takev@student.fontys.nl>
2021-11-21 19:30:00 +0100

    Get my posts in the frontend calling correct API requests + error handling

--- fe-feedbackapp/src/Components/MyPosts.js
+++ fe-feedbackapp/src/Components/MyPosts.js
@@ -1,28 +1,58 @@
 import React, {useState, useEffect} from 'react';
 import { ListGroup } from 'react-bootstrap';
 import styles from "./css/ViewPosts.scss";
+import axios from "axios";
+
 
 function MyPosts(){
 
-    const [selectPost, setSelectPost] = useState("1");
+    const [myPosts, setMyPosts] = useState([]);
+    const [apiNotLoaded, setApiNotLoaded] = useState(false);    
 
-    const Select=(a)=> {
-        setSelectPost(a)
+    useEffect(() => {
+        // todo: get the logged user
+        let idOP = 1;
+        getMyPosts(idOP);
+    }, []);
+
+    async function getMyPosts(idOP) {
+        // get my posts with GET request from the API
+        try{
+            const response = await axios.get('http://localhost:8080/post', { params: { idOP: idOP } });
+            setMyPosts(response.data);
+        }
+        catch(e){
+            console.log(e);
+            setApiNotLoaded(true);
+        }
+        
     }
 
-    
-    
+    const [selectedPost, setSelectedPost] = useState();
+
+    const openSelectedPost =(post) => {
+        setSelectedPost(post);
+        console.log(post.postId);
+    }
+
     return(
         <>
             <ListGroup>
-                <ListGroup.Item className="activePost" >My Post1</ListGroup.Item>
-                <ListGroup.Item className="activePost" > My Post2</ListGroup.Item>
-                <ListGroup.Item className="activePost" > My Post2</ListGroup.Item>
-                <ListGroup.Item className="activePost" > My Post2</ListGroup.Item>
-                <ListGroup.Item className="activePost" >My Post1</ListGroup.Item>
-                <ListGroup.Item className="activePost" > My Post2</ListGroup.Item>
-                <ListGroup.Item className="activePost" > My Post2</ListGroup.Item>
-                <ListGroup.Item className="activePost" > My Post2</ListGroup.Item>
+                {myPosts.length > 0 ? (
+                    myPosts.map((post) => (
+                        <ListGroup.Item className="activePost" onClick={() => openSelectedPost(post)}>{post.title} [{post.postDate}]</ListGroup.Item>
+                    ))
+                ) : 
+                (
+                    apiNotLoaded ?
+                    (
+                        <p>Server error. Please reload!</p>
+                    )
+                    :
+                    (
+                        <p>You do not have any created posts</p>
+                    )
+                )}
                 
             </ListGroup>
         </>
