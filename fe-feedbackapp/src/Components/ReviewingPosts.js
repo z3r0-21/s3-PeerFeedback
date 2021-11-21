a03fab64e08475843f878b81d2dfd047e1debd4e
Junjie Cen <439102@student.fontys.nl>
2021-11-21 20:20:42 +0100

    reviewer's post display in fe from be

--- fe-feedbackapp/src/Components/ReviewingPosts.js
+++ fe-feedbackapp/src/Components/ReviewingPosts.js
@@ -1,14 +1,41 @@
+import axios from 'axios';
 import React, {useState, useEffect} from 'react';
 import { ListGroup } from 'react-bootstrap';
+import { useParams } from 'react-router';
 import styles from "./css/ViewPosts.scss";
 
 function ReviewingPost(){
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() =>{
+        let reviewerId = 5;
+        getPosts(reviewerId);
+    },[]
+
+    )
+
+    async function getPosts(reviewerId){
+        try{
+          const response = await axios.get('http://localhost:8080/post/postsToReview', 
+          {params:{reviewerId:reviewerId}})
+          setPosts(response.data)
+        }
+        catch(e){
+            console.log("no post find for this reviewer")
+        }
+    }
+
     return(
         <>
 
             <ListGroup>
-                <ListGroup.Item className="activePost">other Post1</ListGroup.Item>
-                <ListGroup.Item className="activePost">other Post2</ListGroup.Item>
+                
+                {posts.map((post)=>(
+                    <ListGroup.Item className = "activePost" >
+                        {post.title}
+                    </ListGroup.Item>
+                ))}
             </ListGroup>
         </>
     )
